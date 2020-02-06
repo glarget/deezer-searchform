8834180bf6a56ac9fe220c16b9e4591f2ed90891
LARGET Guillaume <guillaume.larget@adevinta.com>
2020-02-06 11:05:49 +0100

    fix(search): add redux devtools extension for only chrome

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -3,10 +3,11 @@ import thunk from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 
 export default function configureStore(initialState= {}) {
- return createStore(
-  rootReducer,
-  initialState,
-  compose(applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
- )
+    return createStore(
+      rootReducer,
+      initialState,
+      compose(applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : f => f))
 }
